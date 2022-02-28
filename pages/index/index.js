e96e250fa91ea6721aa89ba71db4d8eca331eb47
yimingyu1 <371044760@qq.com>
2022-02-28 08:49:19 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,75 @@
+// pages/index/index.js
+import request from '../../utils/request'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bannerList: [],
+    recommendList: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: async function (options) {
+    let result = await request('/banner', {type: 2})
+    this.setData({
+      bannerList: result.banners
+    })
+    let recommendData = await request('/personalized', {limit: 10})
+    this.setData({
+      recommendList: recommendData.result
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
